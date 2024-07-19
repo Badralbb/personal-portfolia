dbc056887a80f2d584a0ef0d922cc02fa47fc25c
Badralbb <badralbaigalmaa7@gmail.com>
2024-07-19 17:41:17 +0800

    1

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 
+import { Head } from '@/components/Head';
 import { AwesomeButton } from 'react-awesome-button';
 import 'react-awesome-button/dist/styles.css';
 export default function Home() {
   return (
-    <main>
-
-    <AwesomeButton>button</AwesomeButton>
+    <main className='px-4'>
+      <section>
+      <Head/>
+      </section>
+    {/* <AwesomeButton>button</AwesomeButton> */}
     </main>
 
 
