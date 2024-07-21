6a17bab0ad44841e80bfbf78bc3c5916430e34b0
Badralbb <badralbaigalmaa7@gmail.com>
2024-07-21 20:24:32 +0800

    1

--- components/Work.js
+++ components/Work.js
@@ -69,13 +69,13 @@ export const Work = () => {
 
 function Works() {
   return (
-    <div>
+    <div className="rounded-xl">
       {skills.map((item, index) => (
-        <div key={index}>
-          <div className="p-8 bg-[#F9FAFB] mix-w-[279px] max-w-[480px]">
+        <div className="md:flex" key={index}>
+          <div className="p-8 bg-[#F9FAFB] mix-w-[343px] max-w-[576px] md:w-[50%]">
             <Image
               className="w-full"
-              width={270}
+              width={480}
               height={100}
               src={"/images/Picture.png"}
             />
@@ -83,9 +83,42 @@ function Works() {
           <div className="p-8 flex flex-col gap-6">
             <h2>{item.title}</h2>
             <p>{item.text}</p>
-            {skills.map((item, index) => (
-              <div className="flex gap-2">{item.skill}</div>
-            ))}
+            <div className="flex gap-2 flex-wrap">
+              {item.skill.map((item, index) => (
+                <div className="bg-[#E5E7EB] px-6 py-1 rounded-xl" key={index}>
+                  {item}
+                </div>
+              ))}
+            </div>
+            <svg
+              width="36"
+              height="36"
+              viewBox="0 0 36 36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M24 19V25C24 25.5304 23.7893 26.0391 23.4142 26.4142C23.0391 26.7893 22.5304 27 22 27H11C10.4696 27 9.96086 26.7893 9.58579 26.4142C9.21071 26.0391 9 25.5304 9 25V14C9 13.4696 9.21071 12.9609 9.58579 12.5858C9.96086 12.2107 10.4696 12 11 12H17"
+                stroke="#4B5563"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M21 9H27V15"
+                stroke="#4B5563"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M16 20L27 9"
+                stroke="#4B5563"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
           </div>
         </div>
       ))}
